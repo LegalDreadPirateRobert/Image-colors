0dc1a71e305b1916af2893dd38acab49fdaef915
AngularNinjaAvenger <osayibishop@gmail.com>
2020-08-16 08:00:46 +0100

    extracting color name

--- index.js
+++ index.js
@@ -2,10 +2,15 @@ const fs = require('fs')
 const path = require('path')
 const buffer = fs.readFileSync(path.join(__dirname, 'test.jpg'))
 const getColors = require('get-image-colors')
+const { GetColorName }  = require('hex-color-to-color-name');
+const namedColors = require('color-name-list'); 
 
 getColors(buffer,'image/jpg').then(colors => {
     colors.map(color => {
         console.log(color.hex())
+        
+        console.log(GetColorName(color.hex()))
+        
         console.log(color.css())
     })
 })
