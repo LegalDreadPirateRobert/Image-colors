6e25ef7cf9d2e52020ccd84e6e9c8e0784b893b4
AngularNinjaAvenger <osayibishop@gmail.com>
2020-08-22 16:43:33 +0100

    restructured code to OOP

--- index.js
+++ index.js
@@ -1,45 +1,106 @@
-var Table = require('cli-table');
-const chalk = require('chalk');
-
-const white = (text) => chalk.bold.rgb(255, 255, 255)(text);
-var table = new Table({
-    head: [
-        white('Color'),
-        white('Hex'),
-        white('Rgb'),
-    ]
-});
-
 const fs = require('fs')
 const path = require('path')
-const buffer = fs.readFileSync(path.join(__dirname, 'test.jpg'))
+var Table = require('cli-table');
+const chalk = require('chalk');
 const getColors = require('get-image-colors')
 const { GetColorName }  = require('hex-color-to-color-name');
-const namedColors = require('color-name-list'); 
+const  { DEFAULT }  = require("./constant");
 
 
-function rgbColors(rgb){
-    return rgb
-            .split("")
-            .slice(4)
-            .reverse()
-            .slice(1)
-            .reverse()
-            .join("")
-            .split(",");
+class ImageColor {
+    
+    constructor(){
+        this.colorCount = 0;
+        this.table = [];
+        this.options = [];
+        this.getFile();
+        this.getColor();
+    }
+
+    getFile(){
+        const args = process.argv;
+        const options = this.verifyArgument(args);
+        return this.configure();
+    }
+
+    configure(){
+        const white = (text) => chalk.bold.rgb(255, 255, 255)(text);
+        var table = new Table({
+            head: [
+                white('Color'),
+                white('Hex'),
+                white('Rgb'),
+            ]
+        });
+        this.table = table;
+    }
+    
+    rgbColors(rgb){
+        return rgb
+                .split("")
+                .slice(4)
+                .reverse()
+                .slice(1)
+                .reverse()
+                .join("")
+                .split(",");
+    }
+
+    getColor(){
+        var stats = fs.statSync(this.options.filePath);
+        if(stats.isDirectory() && !stats.isFile())this.throwError()
+        
+        fs.readFile(this.options.filePath, (function(err, image) {
+            if (err) throw err;
+            const options  = this.options;
+            getColors(image,options).then((colors,err) => {
+                colors.map(color => {
+                    const c = this.rgbColors(color.css())
+                    const colorText = (text) => chalk.rgb(c[0],c[1], c[2]).bold(text); 
+                    const hex = color.hex();
+                    const rgb = color.css();
+                    const name = GetColorName(color.hex()); 
+                    this.table.push([colorText(name),colorText(hex),colorText(rgb)]);
+                });
+                console.log(this.table.toString())
+            }).catch((err)=>console.log(err))
+        }).bind(this));
+    }
+
+    verifyArgument(args){
+        if(args.length < 3 ) throw new Error("file not specificed");
+        const filePath = args[2];
+
+        const options = {
+            filePath,
+            count:this.getCount(args),
+            type:this.getFileType(filePath)
+        };
+        this.options = options;
+    }
+    
+    getFileType(file){
+        const idx = file.split("").reverse().indexOf(".")
+        if(idx == -1)this.throwError();
+        const fileType = file.split("").slice().reverse().slice(0,idx).reverse().join("");
+        switch(fileType){
+            case "jpg":
+                return "image/jpg";
+            case "png":
+                return "image/png";
+            default:
+                this.throwError();
+        }
+        this.throwError();
+    }
+
+    getCount(args){
+        const count = args.length > 3 && Number(args[3]) != NaN ? Number(args[3]):DEFAULT;
+        return count;
+    }
+    throwError(){
+        throw new Error(" ( file ) does not exist")
+    }
 }
-const options = {
-    count:20,
-    type:'image/jpg'
-}
-getColors(buffer,options).then((colors,err) => {
-    colors.map(color => {
-        const c = rgbColors(color.css())
-        const colorText = (text) => chalk.rgb(c[0],c[1], c[2]).bold(text); 
-        const hex = color.hex();
-        const rgb = color.css();
-        const name = GetColorName(color.hex()); 
-        table.push([colorText(name),colorText(hex),colorText(rgb)]);
-    });
-    console.log(table.toString());
-}).catch((err)=>console.log(err))
+
+new ImageColor();
